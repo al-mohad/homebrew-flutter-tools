1bcac9311e72b18f4e37696e88144a3e1ddf6b46
Muhammad Buhari <49471639+al-mohad@users.noreply.github.com>
2025-03-05 08:37:06 +0100

    fix

--- Formula/create-flutter-app.rb
+++ Formula/create-flutter-app.rb
@@ -1,13 +1,12 @@
-
 class CreateFlutterApp < Formula
-    desc "A script to quickly set up a new Flutter project"
+    desc "🚀 A script to quickly set up a new Flutter project"
     homepage "https://github.com/al-mohad/homebrew-flutter-tools"
-    url "https://github.com/al-mohad/homebrew-flutter-tools/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "REPLACE_WITH_SHA256_CHECKSUM"
+    url "https://github.com/al-mohad/flutter-tools/releases/download/v1.0.0/create-flutter-app-1.0.0.tar.gz"
+    sha256 "b09b3bf4cdad6db0af741d08540a656ead9aed2772d9954091ec242926e43dfc"
     version "1.0.0"
 
     def install
-      bin.install "bin/create_flutter_app"
+      bin.install "create_flutter_app.sh"
     end
 
     test do
